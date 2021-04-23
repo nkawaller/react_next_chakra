3bc5539caf5de2f3376d3cd8de3da8bab99c5ea8
Nathan Kawaller <kawaller@gmail.com>
2021-04-23 09:42:11 -0400

    login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../theme'
 import {createClient, Provider } from 'urql'
 
@@ -9,17 +9,11 @@ const client = createClient({
   }
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
     <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
         <Component {...pageProps} />
-      </ColorModeProvider>
     </ChakraProvider>
     </Provider>
   )
