04d224a925578f35c9c962ac7638d32442111b33
Nathan Kawaller <kawaller@gmail.com>
2021-04-23 08:34:12 -0400

    set up code generator for custom types

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,36 +10,18 @@ import {
 } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-mutation Register($username: String!, $password: String!) {
-    register(options: {username: $username, password: $password}) {
-      errors {
-        field
-        message
-      }
-      user {
-        id
-        createdAt
-        updatedAt
-        username
-      }
-    }
-  }
-`
-
 const Register: React.FC<registerProps> = ({}) => {
-    const [,register] = useMutation(REGISTER_MUTATION)
+    const [,register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-          return register(values);
+        onSubmit={async (values) => {
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => (
