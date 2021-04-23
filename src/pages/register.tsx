9465d57622580b37cd76c6dd1178caa221989f62
Nathan Kawaller <kawaller@gmail.com>
2021-04-23 02:04:02 -0400

    formik username field

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Form, Formik } from "formik";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-        return (
-            <div>register page</div>
-        );
-}
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({ values, handleChange }) => (
+        <Form>
+          <FormControl>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              value={values.username}
+              onChange={handleChange}
+              id="username"
+              placeholder="Username"
+            />
+            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+          </FormControl>
+        </Form>
+      )}
+    </Formik>
+  );
+};
 
-export default Register
+export default Register;
