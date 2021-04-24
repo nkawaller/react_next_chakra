3f52e3be15b02d086f437328392217727714f1e4
Nathan Kawaller <kawaller@gmail.com>
2021-04-24 10:14:51 -0400

    get posts resolver

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,22 @@ import { withUrqlClient } from "next-urql";
 import React from "react";
 import { createUrqlClient } from "../../utils/createUrqlClient";
 import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <div>Hello, World!</div>
-  </>
-);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <Navbar />
+      <div>Hello, World!</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
 
-export default withUrqlClient(createUrqlClient)(Index);
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
