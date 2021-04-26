dc2b0dece99db2c9bcaef3d52fc407370723551e
Nathan Kawaller <kawaller@gmail.com>
2021-04-26 07:35:46 -0400

    create post page

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,41 @@
+import { Box, Button, Textarea } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import React from "react";
+import { InputField } from "../components/InputField";
+import { Wrapper } from "../components/Wrapper";
+
+const CreatePost: React.FC<{}> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="title" placeholder="title" label="Title" />
+            <Box mt={4}>
+              <Textarea
+                name="text"
+                placeholder="write here..."
+                label="Body"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Create Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default CreatePost;
