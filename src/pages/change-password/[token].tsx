f49ebce957eabfe97a567700f07682013c813ae3
Nathan Kawaller <kawaller@gmail.com>
2021-04-26 03:02:38 -0400

    fragments

--- src/pages/change-password/[token].tsx
+++ src/pages/change-password/[token].tsx
@@ -1,14 +1,55 @@
-import { NextPage } from 'next';
-import React from 'react'
+import { Box, Button } from "@chakra-ui/react";
+import { Formik, Form } from "formik";
+import { NextPage } from "next";
+import router from "next/router";
+import React from "react";
+import { toErrorMap } from "../../../utils/toErrorMap";
+import { InputField } from "../../components/InputField";
+import { Wrapper } from "../../components/Wrapper";
+import login from "../login";
 
-const ChangePassword: NextPage<{token: string}> = ({token}) => {
-        return (<div>token is: {token}</div>);
-}
+const ChangePassword: NextPage<{ token: string }> = ({ token }) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ newPassword: '' }}
+        onSubmit={async (values, { setErrors }) => {
+        //   const response = await login(values);
+        //   if (response.data?.login.errors) {
+        //     setErrors(toErrorMap(response.data.login.errors));
+        //   } else if (response.data?.login.user) {
+        //     // worked
+        //     router.push("/");
+        //   }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="newPassword"
+              placeholder="enter new password"
+              label="New Password"
+              type="password"
+            />
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              Change Password
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
 
-ChangePassword.getInitialProps = ({query}) => {
-    return {
-        token: query.token as string
-    }
-}
+ChangePassword.getInitialProps = ({ query }) => {
+  return {
+    token: query.token as string,
+  };
+};
 
-export default ChangePassword
+export default ChangePassword;
