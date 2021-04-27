f9743635eaf87baad67b021d3d8d0d8f6d98a14f
Nathan Kawaller <kawaller@gmail.com>
2021-04-27 09:46:01 -0400

    load more posts button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,32 +4,45 @@ import { createUrqlClient } from "../../utils/createUrqlClient";
 import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import NextLink from "next/link";
-import { Box, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10
     }
   });
+
+  if (!fetching && !data) {
+    return <div>query failed!</div>
+  }
+
+
   return (
     <Layout>
+      <Flex align='center'>
+        <Heading>RedditClone</Heading>
       <NextLink href='create-post'>
-        <Link>create post</Link>
+        <Link ml='auto'>create post</Link>
       </NextLink>
+      </Flex>
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-        {data.posts.map((p) => (
+        {data!.posts.map((p) => (
            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
            <Heading fontSize="xl">{p.title}</Heading>
-           <Text mt={4}>{p.text.slice(0,50)}</Text>
+           <Text mt={4}>{p.textSnippet}</Text>
          </Box>
         ))}
         </Stack>
       )}
+      {data ? <Flex>
+
+      <Button isLoading={fetching} m='auto' my={8}>Load More Posts</Button>
+      </Flex> : null}
     </Layout>
   );
 };
