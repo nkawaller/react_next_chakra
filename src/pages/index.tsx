ee208fdbc6088e6935b3fd6a9d6f4c0658838eab
Nathan Kawaller <kawaller@gmail.com>
2021-04-28 21:50:27 -0400

    working on cursor pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ import { Layout } from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import NextLink from "next/link";
 import { Box, Button, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Index = () => {
+  const [variables, setVariables] = useState({limit: 10, cursor: null as null | string})
   const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10
-    }
+    variables,
   });
 
   if (!fetching && !data) {
@@ -41,7 +41,12 @@ const Index = () => {
       )}
       {data ? <Flex>
 
-      <Button isLoading={fetching} m='auto' my={8}>Load More Posts</Button>
+      <Button onClick={() => {
+        setVariables({
+          limit: variables.limit,
+          cursor: data.posts[data.posts.length - 1].createdAt,
+        })
+      }}isLoading={fetching} m='auto' my={8}>Load More Posts</Button>
       </Flex> : null}
     </Layout>
   );
