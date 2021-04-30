1c5d6d3c3c4daed50427a8e2498b639147c0189c
Nathan Kawaller <kawaller@gmail.com>
2021-04-29 22:16:30 -0400

    reload cache for all pages

--- utils/createUrqlClient.ts
+++ utils/createUrqlClient.ts
@@ -2,6 +2,7 @@ import {
   dedupExchange,
   Exchange,
   fetchExchange,
+  Query,
   stringifyVariables,
 } from "urql";
 import { cacheExchange, Resolver } from "@urql/exchange-graphcache";
@@ -55,7 +56,7 @@ const cursorPagination = (): Resolver => {
     });
 
     return {
-      __typename: 'PaginatedPosts',
+      __typename: "PaginatedPosts",
       hasMore,
       posts: results,
     };
@@ -81,8 +82,12 @@ export const createUrqlClient = (ssrExchange: any) => ({
       updates: {
         Mutation: {
           createPost: (_result, args, cache, info) => {
-            cache.invalidate('Query', 'posts', {
-                limit: 15
+            const allFields = cache.inspectFields('Query');
+            const fieldInfos = allFields.filter(
+              (info) => info.fieldName === 'posts'
+            );
+            fieldInfos.forEach((fi) => {
+            cache.invalidate("Query", "posts", fi.arguments || {});
             })
           },
           logout: (_result, args, cache, info) => {
