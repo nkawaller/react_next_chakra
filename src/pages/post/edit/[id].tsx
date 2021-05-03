75c14a4ebc710bcca0c0e4ae55d70d25f791f267
Nathan Kawaller <kawaller@gmail.com>
2021-05-02 22:35:09 -0400

    update post finished

--- src/pages/post/edit/[id].tsx
+++ src/pages/post/edit/[id].tsx
@@ -1,14 +1,24 @@
 import { Box, Button, Textarea } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
 import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
 import React from "react";
 import { createUrqlClient } from "../../../../utils/createUrqlClient";
-import { useGetPostFromUrl } from "../../../../utils/useGetPostFromUrl";
+import { useGetIntId } from "../../../../utils/useGetIntId";
 import { InputField } from "../../../components/InputField";
 import { Layout } from "../../../components/Layout";
+import { usePostQuery, useUpdatePostMutation } from "../../../generated/graphql";
 
 export const EditPost = ({}) => {
-  const [{ data, fetching }] = useGetPostFromUrl();
+    const router = useRouter();
+    const intId = useGetIntId();
+  const [{ data, fetching }] = usePostQuery({
+      pause: intId === -1,
+      variables: {
+          id: intId,
+      }
+  });
+  const [, updatePost] = useUpdatePostMutation()
   if (fetching) {
     return (
       <Layout>
@@ -29,10 +39,8 @@ export const EditPost = ({}) => {
       <Formik
         initialValues={{ title: data.post.title, text: data.post.text }}
         onSubmit={async (values) => {
-          //   const { error } = await createPost({ input: values });
-          //   if (!error) {
-          //     router.push("/");
-          //   }
+          await updatePost({id: intId, ...values})
+          router.push('/')
         }}
       >
         {({ isSubmitting }) => (
--- utils/useGetPostFromUrl.ts
+++ utils/useGetPostFromUrl.ts
@@ -1,10 +1,8 @@
-import { useRouter } from "next/router";
 import { usePostQuery } from "../src/generated/graphql";
+import { useGetIntId } from "./useGetIntId";
 
 export const useGetPostFromUrl = () => {
-
-    const router = useRouter();
-    const intId = typeof router.query.id === 'string' ? parseInt(router.query.id) : -1;
+const intId = useGetIntId();
     return usePostQuery({
         pause: intId === -1,
         variables: {
